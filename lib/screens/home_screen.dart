402df51468a2d28911e39ddeae01ce91a0ed6ece
teayeon <ta2ye0n@pusan.ac.kr>
2025-05-09 14:47:55 +0900

    캘린더가 보이는데 맞는지는 모르겠음

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import '../auth/login_screen.dart';
+import 'auth/login_screen.dart';
 import '../calendar.dart';
 
 class HomeScreen extends StatelessWidget {
